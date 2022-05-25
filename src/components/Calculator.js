34f446a24be183df9bd4ad937229cdeee74be05b
Andres Agudelo <anagudelogu@gmail.com>
2022-05-25 14:04:02 -0500

    Add handleClick function to lift Button click up

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import calculate from '../js/calculate';
 import Button from './Button';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      calc: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
     this.operationsRight = ['÷', 'x', '-', '+', '='];
     this.operationsCenter = ['AC', '+/-', '%'];
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState((state) => ({
+      calc: calculate(state.calc, buttonName),
+    }));
   }
 
   createDigits() {
     this.digits = [];
 
     for (let i = 0; i < 10; i += 1) {
-      this.digits.push(<Button classN="btn digits__btn" value={i} />);
+      this.digits.push(
+        <Button
+          classN="btn digits__btn"
+          value={`${i}`}
+          handleClick={this.handleClick}
+        />,
+      );
     }
     return this.digits;
   }
@@ -22,18 +42,26 @@ export default class Calculator extends React.Component {
     this.operationsArr = [];
     operations.forEach((operation) => {
       this.operationsArr.push(
-        <Button classN="btn operation__btn" value={operation} />,
+        <Button
+          classN="btn operation__btn"
+          value={operation}
+          handleClick={this.handleClick}
+        />,
       );
     });
     return this.operationsArr;
   }
 
   render() {
+    const { calc } = this.state;
+    const { total, next } = calc;
     return (
       <div className="calculator">
         <div className="calculator__display">
-          <span className="calculator__temp">(0)</span>
-          <span className="calculator__value"> 0 </span>
+          <span className="calculator__value">
+            {' '}
+            {next || total || '0'}
+          </span>
         </div>
 
         <div className="calculator__keypad">
@@ -47,7 +75,11 @@ export default class Calculator extends React.Component {
 
           <div className="digits">
             {this.createDigits()}
-            <Button classN="btn digits__btn" value="." />
+            <Button
+              classN="btn digits__btn"
+              value="."
+              handleClick={this.handleClick}
+            />
           </div>
         </div>
       </div>

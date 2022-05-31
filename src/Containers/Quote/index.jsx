2239bb9211521e28c9e41df60a158b0c34cb873e
Andres Agudelo <anagudelogu@gmail.com>
2022-05-31 15:49:20 -0500

    Add fully responsive quote page

---
+++ src/Containers/Quote/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Quote.css';
+
+const Quote = () => (
+  <div className="quote">
+    <p className="quote__text">
+      Mathematics is not about numbers, equations, computations, or
+      algorithms: it is about understanding.
+    </p>
+    <p className="quote__author">- William Paul Thurston</p>
+  </div>
+);
+
+export default Quote;

cdfb1fac53ebb914d185aa3af6bffdb409881ce7
Andres Agudelo <anagudelogu@gmail.com>
2022-05-31 09:49:12 -0500

    Refactor Calculator to have it's own styles and be in Pages folder

---
+++ src/Pages/Calculator/Calculator.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
+import Button from '../../components/Button';
+import './Calculator.css';
+import * as keys from './logic/keys';
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const { total, next } = state;
+
+  const handleClick = (buttonName) => {
+    setState((prevState) => calculate(prevState, buttonName));
+  };
+
+  const createDigits = (digits) => {
+    const digitsBtns = digits.map((digit) => (
+      <Button
+        key={`${digit}`}
+        isOperation={false}
+        value={`${digit}`}
+        handleClick={handleClick}
+      />
+    ));
+    return digitsBtns;
+  };
+
+  const createOperations = (operations) => {
+    const operationsArr = operations.map((operation) => (
+      <Button
+        key={operation}
+        isOperation
+        value={operation}
+        handleClick={handleClick}
+      />
+    ));
+    return operationsArr;
+  };
+
+  return (
+    <div className="calculator">
+      <div className="calculator__display">
+        <span className="calculator__value">
+          {' '}
+          {next || total || '0'}
+        </span>
+      </div>
+
+      <div className="calculator__keypad">
+        <div className="operations right">
+          {createOperations(keys.OPERATIONS_RIGHT)}
+        </div>
+
+        <div className="operations center">
+          {createOperations(keys.OPERATIONS_CENTER)}
+        </div>
+
+        <div className="digits">{createDigits(keys.DIGITS)}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
---
+++ src/Pages/Calculator/logic/keys.js
@@ -0,0 +1,17 @@
+const OPERATIONS_RIGHT = ['÷', 'x', '-', '+', '='];
+const OPERATIONS_CENTER = ['AC', '+/-', '%'];
+const DIGITS = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '.',
+];
+
+export { OPERATIONS_CENTER, OPERATIONS_RIGHT, DIGITS };

81a0a5cf4e0a1cd4c4cee3b0a2ced46dd2f83666
Andres Agudelo <anagudelogu@gmail.com>
2022-06-01 10:11:41 -0500

    Add tests to Button component

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,22 @@
+import Button from './index';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Button', () => {
+  it('Renders', () => {
+    render(<Button />);
+    expect(screen.getByRole('button')).not.toBeNull();
+  });
+
+  it('Renders a Button component', () => {
+    render(<Button />);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('If the Button isOperation, adds a class operation__btn', () => {
+    render(<Button isOperation />);
+    expect(screen.getByRole('button').className).toMatch(
+      /operation__btn/,
+    );
+  });
+});
